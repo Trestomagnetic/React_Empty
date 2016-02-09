5c79830f9dd0760b97802c940134dcd57a1b51e0
trestin bradley <trestin.bradley@SkyKick.com>
2016-02-09 10:17:46 -0500

    First commit
    just npm install to get started

---
+++ src/components/common/header.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
+
+var Header = React.createClass({
+  render: function() {
+    return (
+      <nav className="navbar navbar-default">
+        <div ckassName="container-fluid">
+          <ul className="nav navbar-nav">
+            <li><Link to="app">Home</Link></li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+});
+
+module.exports = Header;
